91d052346522e2361e06fdf92cbc6e03ee703ca0
skidrowmega <shilihin_2005@mail.ru>
2026-09-12 21:23:57 +0700

    Поправил анимации, Добавил фреймворк для отталкиваний.

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -1,24 +1,55 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Accessibility;
 
-public class CharacterController : MonoBehaviour
+public class CharacterController : Entity
 {
-    public float Speed;
     public Transform Cursor;
     Vector3 CursorVector;
-    void Start()
+    Animator animator;
+
+
+
+
+
+
+    private void Awake()
     {
-        
+        animator= GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        Move();
+        LookAtCursor();
+        Attack();
+        PushPerFrame();
+        if (Input.GetMouseButtonUp(1))
         {
-            transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Speed;
+            PushEntity(new Vector3(5, 1, 7), 0.3f);
         }
-        CursorVector =  Cursor.position- transform.position;
+    }
+
+    void Attack()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+        if (animator == null) return;
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("VESLOBASHLEFT")) return;
+        animator.Play("VESLOBASHLEFT");
+    }
+    void Move()
+    {
+        if (IsStunned) return;
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Speed;
+    }
+    void LookAtCursor()
+    {
+        CursorVector = Cursor.position - transform.position;
         transform.rotation = Quaternion.Euler(0, Mathf.Rad2Deg * Mathf.Atan2(-CursorVector.z, CursorVector.x), 0);
     }
+
+
+
 }
--- Assets/Scripts/WeaponMelee.cs
+++ Assets/Scripts/WeaponMelee.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public abstract class WeaponMelee: MonoBehaviour
 {
-    public string WeaponName;
-    public int Damage;
-    public int Stamina;
-    public int StaminaRecoveryRate;
-    public int StamineDecreaseRate;
+    public string WeaponName="Oar";
+    public int Damage=1;
+    public int Stamina=100;
+    public int StaminaRecoveryRate=20;
+    public int StamineDecreaseRate=20;
+    public int PushTime=0;
 
     public virtual void Punch() {
     
