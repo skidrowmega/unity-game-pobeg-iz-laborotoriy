bafbbdd593027e3e14c58da1abd6270bb03c796c
skidrowmega <shilihin_2005@mail.ru>
2026-09-26 19:45:53 +0700

    Починил курсор

--- Assets/Scripts/Player/FOLLOWCURSOR.cs
+++ Assets/Scripts/Player/FOLLOWCURSOR.cs
@@ -5,13 +5,18 @@ public class FOLLOWCURSOR : MonoBehaviour
     public MeshCollider Plane;
     public LineRenderer linerenderer;
     public Entity player;
+    private float Pivot;
     // Update is called once per frame
+    private void Start()
+    {
+        Pivot=Plane.transform.position.y+.02f;
+    }
     void Update()
     {
         RaycastHit hit;
         Plane.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100);
         if (hit.collider)
-            transform.position = new Vector3(hit.point.x, 2.242f, hit.point.z);
+            transform.position = new Vector3(hit.point.x, Pivot, hit.point.z);
         if (Input.GetMouseButton(1))
         {
             if (!linerenderer.enabled)
