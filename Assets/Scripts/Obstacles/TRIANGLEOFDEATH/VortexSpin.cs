d654c950593702b522dc7e04f8c60e2f962123fa
skidrowmega <shilihin_2005@mail.ru>
2026-09-26 19:37:45 +0700

    Забабахал крутилки

--- Assets/Scripts/Obstacles/TRIANGLEOFDEATH/VortexSpin.cs
+++ Assets/Scripts/Obstacles/TRIANGLEOFDEATH/VortexSpin.cs
@@ -1,3 +1,8 @@
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
@@ -7,9 +12,9 @@ public class VortexSpin : MonoBehaviour
     [SerializeField] float RotationSpeed = 1;
     [SerializeField] float damage_radius = 1;
     [SerializeField] int Damage = 1000;
+    [SerializeField] int DamageToPlayer = 30;
     [SerializeField] float pushstrength;
     CharacterController player;
-
     void Awake()
     {
         player = GameObject.Find("BOBR").GetComponent<CharacterController>();
@@ -25,13 +30,29 @@ public class VortexSpin : MonoBehaviour
 
     void DamagePlayer()
     {
-        //player.TakeDamage(Damage, transform.position, null, 0, DamageType.Unparriable);
+        VortexSpin[] vortices = GetVortices();
+        VortexSpin randomvortex=vortices[Random.Range(0, vortices.Length)];
+        Vector3 newpos = player.transform.position;
+        newpos.x = randomvortex.transform.position.x;
+        newpos.z = randomvortex.transform.position.z;
+        player.transform.position = newpos;
+        player.TakeDamage(DamageToPlayer, transform.position, null, 0, DamageType.Unparriable);
+        DestroyBothVortices(randomvortex);
+
     }
     void DamageEnemy(Entity target)
     {
+        VortexSpin[] vortices = GetVortices();
+        VortexSpin randomvortex = vortices[Random.Range(0, vortices.Length)];
         target.TakeDamage(Damage, transform.position, null, 0, DamageType.Unparriable);
+        Destroy(gameObject);
     }
 
+    void DestroyBothVortices(VortexSpin teleporttarget)
+    {
+        Destroy(teleporttarget.gameObject);
+        Destroy(gameObject);
+    }
 
     void CheckCollision()
     {
@@ -46,6 +67,14 @@ public class VortexSpin : MonoBehaviour
             else DamageEnemy(target);
         }
         
+    }
+
+    VortexSpin[] GetVortices()
+    {
+        List<VortexSpin> vortices;
+        vortices = FindObjectsByType<VortexSpin>().ToList();
+        vortices.Remove(this);
+        return vortices.ToArray();
     }
 
 
